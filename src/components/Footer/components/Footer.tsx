1a31efeb015e5fe4b7365d2cac02f62e03b3ffc3
ikkiestadeltoya <samuelsedran@gmail.com>
2024-05-09 21:47:46 -0300

    commit

--- src/components/Footer/components/Footer.tsx
+++ src/components/Footer/components/Footer.tsx
@@ -1,51 +1,51 @@
 import { Container } from "@mui/material";
-import FooterList from "./FooterList";
 import {MdFacebook} from 'react-icons/md';
 import {AiFillTwitterCircle, AiFillInstagram, AiFillYoutube} from 'react-icons/ai';
-import FooterStyle from "./css/Footer.module.css";
+import FooterStyle from '../css/Footer.module.css';
+import { Link } from "react-router-dom";
 
 
 const Footer = () => {
-    return <footer className="bg-slate-700 
-    text-slate-200
-    text-sm mt16
-    ">
+    return <footer className={FooterStyle.container}>
+
+
         <Container>
-            <div className="flex flex-col md:flex-row
-            justify-between
-            pt-16
-            pb-8">
-                <FooterList>
-                    <h3 className="text-base font-bold mb-2">Que ofrecemos</h3>
-                    <link href='#'>Alquiler de Salas</link>
-                    <link href='#'>Bar</link>
-                    <link href='#'>Shows</link>
-                    <link href='#'>Clases Personalizadas</link>
-                </FooterList>
-                <FooterList>
-                    <h3 className="text-base font-bold mb-2">Que ofrecemos</h3>
-                    <link href='#'>Servicio al Cliente</link>
-                    <link href='#'>Reclamos</link>
-                    <link href='#'>Contacto</link>
-                    <link href='#'>Politicas</link>
-                </FooterList>
-                <FooterList>
-                    <h3 className="text-base font-bold mb-2">Sobre Nosotros</h3>
-                    <p className="mb-2">Somos El Multiespacio El juvenil
+            <div className={FooterStyle.footer_container}>
+                    
+                    <div className={FooterStyle.footer_element}>
+                    <h3 className={FooterStyle.footer_element}> Que ofrecemos</h3>
+                    <Link  to='#'>Alquiler de Salas</Link>
+                    <Link to='#'>Bar</Link>
+                    <Link to='#'>Shows</Link>
+                    <Link to='#'>Clases Personalizadas</Link>
+                    </div>
+
+                    <div className={FooterStyle.footer_element}>
+                    <h3 className={FooterStyle.footer_element}>Atencion</h3>
+                    <Link to='#'>Servicio al Cliente</Link>
+                    <Link to='#'>Reclamos</Link>
+                    <Link to='#'>Contacto</Link>
+                    <Link to='#'>Politicas</Link>
+                    </div>
+             
+                    <div className={FooterStyle.footer_element}>
+                    <h3 className={FooterStyle.footer_element}>Sobre Nosotros</h3>
+                    <p className={FooterStyle.footer_descripcion}>Somos El Multiespacio El juvenil
                         ofrecemos salas, clases y shows.
                     </p>
-                </FooterList>
-                <FooterList>
-                
-                    <h3 className="text-base font-bold mb-2">Seguinos</h3>
-                    <div className="flex gap-2">
-                    <link href='#'><MdFacebook size = {24}/></link>
-                    <link href='#'><AiFillTwitterCircle size = {24}/></link>
-                    <link href='#'><AiFillInstagram size = {24}/></link>
-                    <link href='#'><AiFillYoutube size = {24}/></link>
                     </div>
                 
-                </FooterList>
+                    <div className={FooterStyle.footer_element}>
+                    <h3 className={FooterStyle.footer_element}>Seguinos</h3>
+                    <div className={FooterStyle.footer_redes}>
+                    <Link to='#'><MdFacebook size = {24}/></Link>
+                    <Link to='#'><AiFillTwitterCircle size = {24}/></Link>
+                    <Link to='#'><AiFillInstagram size = {24}/></Link>
+                    <Link to='#'><AiFillYoutube size = {24}/></Link>
+                    </div>
+                    </div>
+                
+               
             </div>
 
 
